85c6d2710ba10dda2a96bf98182e13bba7ca59d4
domenvilar <domen.vilar@gmail.com>
2024-04-23 19:00:09 +0200

    We can see projects the user belongs to inside Sprint and Product backlog.

--- client/src/components/ProductBacklog.vue
+++ client/src/components/ProductBacklog.vue
@@ -1,74 +1,83 @@
 <template>
-
-<div style="height: 100%;">
-    <div style="margin: 30px auto; max-width: 80%;">
-    <!-- Project Dropdown -->
-    <v-card width="50%">
-
+  <div style="height: 100%">
+    <div style="margin: 30px auto; max-width: 80%">
+      <!-- Project Dropdown -->
+      <v-card width="50%">
         <div v-if="isLoading">Loading...</div>
-        <v-select v-else v-model="selectedProject" :items="projects" label="Select a project" item-title="name"
-            return-object @change="onProjectChange">
+        <v-select
+          v-else
+          v-model="selectedProject"
+          :items="projects"
+          label="Select a project"
+          item-title="name"
+          return-object
+          @change="onProjectChange">
         </v-select>
-    </v-card>
-    <!-- Content that changes based on the selection -->
+      </v-card>
+      <!-- Content that changes based on the selection -->
 
-    <div style="margin: 20px 0;">
+      <div style="margin: 20px 0">
         <div v-if="isLoading"></div>
         <div v-else>
-            <h3>Details for {{ selectedProject.name }}</h3>
-            <p>Description: {{ selectedProject.description }}</p>
-            <!-- Add more project details here -->
-            <v-card>
-                <component :is="StoryWall" :selectedProject="selectedProject" />
-            </v-card>
+          <h3>Details for {{ selectedProject.name }}</h3>
+          <p>Description: {{ selectedProject.description }}</p>
+          <!-- Add more project details here -->
+          <v-card>
+            <component :is="StoryWall" :selectedProject="selectedProject" />
+          </v-card>
         </div>
+      </div>
     </div>
-    </div>
-</div>
-
-
+  </div>
 </template>
-
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
-import { supabase } from "../lib/supabaseClient";
+import { supabase } from '../lib/supabaseClient';
 import StoryWall from './StoryWall.vue';
 onMounted(() => {
-    getProjects();
+  getProjects();
 });
 const isLoading = ref(true);
 const isAdmin = ref(false);
 const projects = ref<any[]>([]);
 supabase.auth.onAuthStateChange(async (_, session) => {
-    if (session) {
-        const jwt = session.access_token;
-        const payload = JSON.parse(atob(jwt.split('.')[1]));
-        isAdmin.value = payload.user_role === 'admin';
-    } else {
-        console.log('The user is not authenticated');
-    }
+  if (session) {
+    const jwt = session.access_token;
+    const payload = JSON.parse(atob(jwt.split('.')[1]));
+    isAdmin.value = payload.user_role === 'admin';
+  } else {
+    console.log('The user is not authenticated');
+  }
 });
 async function getProjects() {
-    const { data, error } = await supabase
-        .from('project')
-        .select('id, name, description')
-    if (error) {
-        console.error('Error fetching projects');
-    } else {
-        projects.value = data;
-        isLoading.value = false;
-        console.log('Projects:', projects.value);
-        console.log(data)
-        selectedProject.value = projects.value[0];
-    }
+
+  const user = (await supabase.auth.getUser()).data.user;
+  const userId = user.id;
+
+  const { data, error } = await supabase
+    .from('project_role')
+    .select(
+      `
+            project:project_id ( id, name, description )
+        `
+    )
+    .eq('user_id', userId);
+
+  if (error) {
+    console.error('Error fetching projects');
+  } else {
+    projects.value = data.map((project: any) => project.project);
+    isLoading.value = false;
+    selectedProject.value = projects.value[0];
+  }
 }
 const selectedProject = ref({
-    id: '',
-    name: '',
-    description: '',
+  id: '',
+  name: '',
+  description: '',
 });
 function onProjectChange() {
-    console.log('Selected project:', selectedProject.value);
+  console.log('Selected project:', selectedProject.value);
 }
 </script>
--- client/src/components/SprintBacklog.vue
+++ client/src/components/SprintBacklog.vue
@@ -263,13 +263,22 @@ async function openDialog(index: number) {
 }
 
 async function getProjects() {
+  const user = (await supabase.auth.getUser()).data.user;
+  const userId = user.id;
+
   const { data, error } = await supabase
-    .from('project')
-    .select('id, name, description')
+    .from('project_role')
+    .select(
+      `
+            project:project_id ( id, name, description )
+        `
+    )
+    .eq('user_id', userId);
+
   if (error) {
     console.error('Error fetching projects');
   } else {
-    projects.value = data;
+    projects.value = data.map((project: any) => project.project);
     isLoading.value = false;
     selectedProject.value = projects.value[0];
   }
