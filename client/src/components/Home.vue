685cc04c870a98ed5af9e36ef5829fe5f18d6bde
tommsojer <tom.sojer2@gmail.com>
2024-04-25 14:04:42 +0200

    Change datetime of login

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -13,59 +13,73 @@ defineProps<{ msg: string }>();
 const router = useRouter();
 
 const signOut = async () => {
+  await setLoginDateTime();
   await supabase.auth.signOut();
   router.push('/login');
 };
+
+async function setLoginDateTime() {
+  const user = await (await supabase.auth.getUser()).data.user;
+
+  const { error } = await supabase
+    .from('user_profile')
+    .update({ last_login: user?.last_sign_in_at })
+    .eq('user_id', user?.id);
+  if (error) {
+    console.log("Error updating last login")
+  }
+}
 </script>
+
 
 <template>
   <v-card>
     <v-layout>
       <v-navigation-drawer class="bg-deep-purple" theme="dark" permanent>
-        <v-list>
-          <v-list-item prepend-icon="mdi-account-circle" @click="showDialog" class="dlgButton">
-            <v-list-title style="font-weight: 600;">{{ userName }}</v-list-title>
-          </v-list-item>
-        </v-list>
-        <v-divider :thickness="4"></v-divider>
-        <v-list>
-          <v-list-item @click="selected = Project" prepend-icon="mdi-view-dashboard">
-            <v-list-title>Projects</v-list-title>
-          </v-list-item>
-          <v-list-item @click="selected = User" prepend-icon="mdi-account-box">
-            <v-list-title>Users</v-list-title>
-          </v-list-item>
-          <!-- <v-list-item
-            @click="selected = StoryWall"
-            prepend-icon="mdi-pencil">
-            <v-list-title>User stories (temporary)</v-list-title>
-          </v-list-item> -->
-          <v-list-item @click="selected = Sprint" prepend-icon="mdi-clock-fast">
-            <v-list-title>Sprints</v-list-title>
-          </v-list-item>
-          <v-list-item @click="selected = ProductBacklog" prepend-icon="mdi-file-tree">
-            <v-list-title>Product backlog</v-list-title>
-          </v-list-item>
-          <v-list-item @click="selected = SprintBacklog" prepend-icon="mdi-invoice-list-outline" v-if="!isProductOwner">
-            <v-list-title>Sprint backlog</v-list-title>
-          </v-list-item>
-          <!-- <v-btn @click="$refs.dlgUserStory.show = true" class="dlgButton">New user story</v-btn>
-          <dlg-new-story ref="dlgUserStory"></dlg-new-story> -->
-        </v-list>
-
-        <DlgProfile v-model="profileDlg" @update:show="profileDlg = $event"></DlgProfile>
-
-        <template v-slot:append>
           <v-list>
-            <v-list-item>Last sign in: <br>
-              {{ formatDateTime(user?.last_sign_in_at ?? '') }}</v-list-item>
+            <v-list-item prepend-icon="mdi-account-circle" @click="showDialog" class="dlgButton">
+              <v-list-title style="font-weight: 600;">{{ userName }}</v-list-title>
+            </v-list-item>
           </v-list>
-          <div class="pa-2">
-            <v-btn block @click="signOut"> Logout </v-btn>
-          </div>
-        </template>
-      </v-navigation-drawer>
+          <v-divider :thickness="4"></v-divider>
+          <v-list>
+            <v-list-item @click="selected = Project" prepend-icon="mdi-view-dashboard">
+              <v-list-title>Projects</v-list-title>
+            </v-list-item>
+            <v-list-item @click="selected = User" prepend-icon="mdi-account-box">
+              <v-list-title>Users</v-list-title>
+            </v-list-item>
+            <!-- <v-list-item
+              @click="selected = StoryWall"
+              prepend-icon="mdi-pencil">
+              <v-list-title>User stories (temporary)</v-list-title>
+            </v-list-item> -->
+            <v-list-item @click="selected = Sprint" prepend-icon="mdi-clock-fast">
+              <v-list-title>Sprints</v-list-title>
+            </v-list-item>
+            <v-list-item @click="selected = ProductBacklog" prepend-icon="mdi-file-tree">
+              <v-list-title>Product backlog</v-list-title>
+            </v-list-item>
+            <v-list-item @click="selected = SprintBacklog" prepend-icon="mdi-invoice-list-outline"
+              v-if="!isProductOwner">
+              <v-list-title>Sprint backlog</v-list-title>
+            </v-list-item>
+            <!-- <v-btn @click="$refs.dlgUserStory.show = true" class="dlgButton">New user story</v-btn>
+            <dlg-new-story ref="dlgUserStory"></dlg-new-story> -->
+          </v-list>
 
+          <DlgProfile v-model="profileDlg" @update:show="profileDlg = $event"></DlgProfile>
+
+          <template v-slot:append>
+            <v-list v-if="isDone">
+              <v-list-item>Last sign in: <br>
+                {{ formatDateTime(lastLogin) }}</v-list-item>
+            </v-list>
+            <div class="pa-2">
+              <v-btn block @click="signOut"> Logout </v-btn>
+            </div>
+          </template>
+      </v-navigation-drawer>
       <v-main>
         <component :is="selected" />
       </v-main>
@@ -80,6 +94,8 @@ import { formatDateTime } from '../lib/dateFormatter';
 
 const profileDlg = ref(false);
 const isProductOwner = ref(false);
+const lastLogin = ref('');
+const isDone = ref(false);
 
 const showDialog = () => {
   profileDlg.value = true;
@@ -107,9 +123,11 @@ async function getProjectRole() {
 async function getUserName() {
   const { data } = await supabase
     .from('user_profile')
-    .select('name, surname')
+    .select('name, surname, last_login')
     .eq('user_id', user?.id)
     .single();
+
+  lastLogin.value = data?.last_login;
 
   return `${data?.name} ${data?.surname}`;
 };
@@ -125,6 +143,7 @@ export default {
   async mounted() {
     this.userName = await getUserName();
     await getProjectRole();
+    isDone.value = true;
   },
 };
 </script>
